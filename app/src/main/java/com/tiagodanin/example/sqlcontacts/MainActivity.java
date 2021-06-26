f86db274070a9a451e3414be213aba0a68adb890
Tiago Danin <TiagoDanin@outlook.com>
2021-06-26 14:53:57 -0300

    fix: remove used import in main

--- app/src/main/java/com/tiagodanin/example/sqlcontacts/MainActivity.java
+++ app/src/main/java/com/tiagodanin/example/sqlcontacts/MainActivity.java
@@ -1,6 +1,5 @@
 package com.tiagodanin.example.sqlcontacts;
 
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -13,7 +12,6 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
 import com.techyourchance.threadposter.BackgroundThreadPoster;
 import com.techyourchance.threadposter.UiThreadPoster;
 import com.tiagodanin.example.sqlcontacts.events.CallbackContactsListAdapter;
